67f35200384fff4192b8e4454e5ef681c1668ca9
Dupré <theo.thws.06@gmail.com>
2025-05-14 01:32:35 +0200

    Setting up Env

--- config/config.php
+++ config/config.php
@@ -1,12 +1,19 @@
 <?php
-// config.php
+// Inclure le fichier autoload de Composer
+require_once '../vendor/autoload.php'; // Assure-toi que le chemin est correct
 
-$host = 'localhost';
-$dbname = 'projet_php_blog';
-$username = 'root';
-$password = ''; // à adapter selon ton environnement
+// Charger les variables d'environnement depuis le fichier .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Récupérer les valeurs des variables d'environnement
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD'); // Peut être vide, selon ton environnement
 
 try {
+    // Créer une connexion à la base de données avec PDO
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     // Définir les erreurs PDO comme exceptions
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
