5a6991ddcbc4781fb7e1a2107a4db2b6486cdcb6
xEsiah-Desktop <theo.thws.06@gmail.com>
2025-05-14 20:56:34 +0200

    Fixing links header

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Esiah's Corner</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="../includes/icon.ico">
+    <link rel="icon" type="image/x-icon" href="/includes/icon.ico">
 
     <link rel="stylesheet" href="/css/index.css">
 
@@ -25,17 +25,17 @@ if (session_status() === PHP_SESSION_NONE) {
             <p>Le forum de l'élitisme</p>
         </div>
         <nav>
-            <a href="../public/index.php">Accueil</a>
+            <a href="/public/index.php">Accueil</a>
             <?php if (isset($_SESSION['user'])): ?>
                 <!-- L'utilisateur est connecté, on affiche le lien d'Administration -->
-                <a href="../admin/article_creation.php">Créer un post</a>
-                <a href="../admin/index.php">Administration</a>
+                <a href="/admin/article_creation.php">Créer un post</a>
+                <a href="/admin/index.php">Administration</a>
 
-                <a href="../public/logout.php">Déconnexion</a>
+                <a href="/public/logout.php">Déconnexion</a>
             <?php else: ?>
                 <!-- L'utilisateur n'est pas connecté, on affiche le lien pour faire une demande d'adhésion -->
-                <a href="../public/request_access.php">Faire une demande d'adhésion</a>
-                <a href="../public/login.php">Se connecter</a>
+                <a href="/public/request_access.php">Faire une demande d'adhésion</a>
+                <a href="/public/login.php">Se connecter</a>
             <?php endif; ?>
         </nav>
     </header>
